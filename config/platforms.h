11d793255857beb8e0f83960ec7da450024540ad
hoangminh88 <65163004+hoangminh88@users.noreply.github.com>
2021-09-08 15:42:34 +0700

    Update platforms.h

--- config/platforms.h
+++ config/platforms.h
@@ -43,6 +43,30 @@ const struct hw_config supported_platforms[] = {
             .emulate_rtc = true,
             .swap_serial = false,
             .reinit_ttyS0 = true,
+            .fix_disk_led_ctrl = true,
+    },
+     {
+            .name = "DS920+",
+            .pci_stubs = {
+                    { .type = VPD_MARVELL_88SE9215,    .bus = 0x01, .dev = 0x00, .fn = 0x00, .multifunction = false },
+                    { .type = VPD_INTEL_I211,          .bus = 0x02, .dev = 0x00, .fn = 0x00, .multifunction = false },
+                    { .type = VPD_INTEL_I211,          .bus = 0x03, .dev = 0x00, .fn = 0x00, .multifunction = false },
+                    { .type = VPD_INTEL_CPU_AHCI_CTRL, .bus = 0x00, .dev = 0x12, .fn = 0x00, .multifunction = false },
+                    { .type = VPD_INTEL_CPU_PCIE_PA,   .bus = 0x00, .dev = 0x13, .fn = 0x00, .multifunction = false },
+                    { .type = VPD_INTEL_CPU_PCIE_PB,   .bus = 0x00, .dev = 0x14, .fn = 0x00, .multifunction = false },
+                    { .type = VPD_INTEL_CPU_USB_XHCI,  .bus = 0x00, .dev = 0x15, .fn = 0x00, .multifunction = false },
+                    { .type = VPD_INTEL_CPU_I2C,       .bus = 0x00, .dev = 0x16, .fn = 0x00, .multifunction = false },
+                    { .type = VPD_INTEL_CPU_HSUART,    .bus = 0x00, .dev = 0x18, .fn = 0x00, .multifunction = false },
+                    { .type = VPD_INTEL_CPU_SPI,       .bus = 0x00, .dev = 0x19, .fn = 0x02, .multifunction = true },
+                    { .type = VPD_INTEL_CPU_SPI,       .bus = 0x00, .dev = 0x19, .fn = 0x00, .multifunction = true },
+                    { .type = VPD_INTEL_CPU_SMBUS,     .bus = 0x00, .dev = 0x1f, .fn = 0x01, .multifunction = true },
+                    { .type = VPD_INTEL_CPU_SMBUS,     .bus = 0x00, .dev = 0x1f, .fn = 0x00, .multifunction = true },
+
+                    { .type = __VPD_TERMINATOR__ }
+            },
+            .emulate_rtc = true,
+            .swap_serial = false,
+            .reinit_ttyS0 = true,
             .fix_disk_led_ctrl = false,
     },
 };
